a3fb89914ae9b3978d1487f0db1088251458c334
Zavala <toro.d.c@gmail.com>
2021-05-09 14:57:37 -0400

    prec route for app complete

--- app.py
+++ app.py
@@ -25,12 +25,24 @@ app = Flask(__name__)
 def home():
     return "Welcome to the home page. Here is a list of available endpoints:<br><br><br>Precipitation data: /api/v1.0/precipitation<br><br>Station data: /api/v1.0/stations<br><br>Temperatures of most active station: /api/v1.0/tobs<br><br>Min, max, and avg temperatures from selected date: /api/v1.0/[date]<br><br>Min, max, and avg temperatures from selected date range: /api/v1.0/[start date]/[end date]"
 
-# @app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitation")
 def prec_handler():
     session = Session(engine)
+    query_date = dt.date(2017,8,23) - dt.timedelta(days=365)
+    meas_data = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= query_date).order_by(Measurement.date).all()
+    # prec_query = session.query(Measurement).all()
+    results_1 = []
+    # results_2 = []
+
+    for row in meas_data:
+        results_1.append({row[0]:row[1]})
+    
+    # for row in results_1:
+    #     results_2.append({row})
 
     session.close()
 
+    return jsonify(results_1)
 
 # @app.route("/api/v1.0/stations")
 
