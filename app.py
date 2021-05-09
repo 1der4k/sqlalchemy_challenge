c2c1137710f7466e6b4239b7fb20f164f625fba6
Zavala <toro.d.c@gmail.com>
2021-05-09 12:45:03 -0400

    imported additional dependencies and coded sqlite db setup

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
+# Dependencies
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+import numpy as np
+import pandas as pd
+import datetime as dt
 
-# endpoint_list = [
-#     {"endpoint" : "/api/v1.0/precipitation",
-#     "description": "precipitation data"
-#     },
-#     {"endpoint" : "/api/v1.0/stations",
-#     "description": "station data"
-#     },
-#     {"endpoint" : "/api/v1.0/tobs",
-#     "description": "temperature observations of most active station"
-#     },
-#     {"endpoint" : "/api/v1.0/<start>",
-#     "description": "minimum, maximum, and average temperatures from this date"
-#     },
-#     {"endpoint" : "/api/v1.0/<start>/<end>",
-#     "description": "minimum, maximum, and average temperatures from this date range"
-#     }
-# ]
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# DB setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+conn = engine.connect()
+
+# App setup
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to the home page. Here is a list of available endpoints:<br><br><br>Precipitation data: /api/v1.0/precipitation<br><br>Station data: /api/v1.0/stations<br><br>Temperatures of most active station: /api/v1.0/tobs<br><br>Min, max, and avg temperatures from selected date: /api/v1.0/<start><br><br>Min, max, and avg temperatures from selected date range: /api/v1.0/<start>/<end>"
+    return "Welcome to the home page. Here is a list of available endpoints:<br><br><br>Precipitation data: /api/v1.0/precipitation<br><br>Station data: /api/v1.0/stations<br><br>Temperatures of most active station: /api/v1.0/tobs<br><br>Min, max, and avg temperatures from selected date: /api/v1.0/[date]<br><br>Min, max, and avg temperatures from selected date range: /api/v1.0/[start date]/[end date]"
 
 # @app.route("/api/v1.0/precipitation")
 
