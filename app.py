12b7571833981e0c46f65ff35e722b0c05ae060e
Zavala <toro.d.c@gmail.com>
2021-05-09 12:02:34 -0400

    additional home page return messaging including endpoint locations and descriptions

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# endpoint_list = [
+#     {"endpoint" : "/api/v1.0/precipitation",
+#     "description": "precipitation data"
+#     },
+#     {"endpoint" : "/api/v1.0/stations",
+#     "description": "station data"
+#     },
+#     {"endpoint" : "/api/v1.0/tobs",
+#     "description": "temperature observations of most active station"
+#     },
+#     {"endpoint" : "/api/v1.0/<start>",
+#     "description": "minimum, maximum, and average temperatures from this date"
+#     },
+#     {"endpoint" : "/api/v1.0/<start>/<end>",
+#     "description": "minimum, maximum, and average temperatures from this date range"
+#     }
+# ]
+
 @app.route("/")
 def home():
-    return "Welcome to the home page."
+    return "Welcome to the home page. Here is a list of available endpoints:<br><br><br>Precipitation data: /api/v1.0/precipitation<br><br>Station data: /api/v1.0/stations<br><br>Temperatures of most active station: /api/v1.0/tobs<br><br>Min, max, and avg temperatures from selected date: /api/v1.0/<start><br><br>Min, max, and avg temperatures from selected date range: /api/v1.0/<start>/<end>"
 
 # @app.route("/api/v1.0/precipitation")
 
